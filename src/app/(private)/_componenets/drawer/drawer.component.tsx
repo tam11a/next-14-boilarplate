5ad8dfe89f1e4df0453e76f0a501d6e5492f4177
asatter833 <asatter833@gmail.com>
2024-09-30 08:09:12 +0600

    basic drawer ready. user details have to be updated when the api is available

--- src/app/(private)/_componenets/drawer/drawer.component.tsx
+++ src/app/(private)/_componenets/drawer/drawer.component.tsx
@@ -1,5 +1,10 @@
-// import Image from 'next/image';
+import Image from 'next/image';
+import { stringAvatar } from '@/lib/string-avatar';
 import Menu from './menu.component';
+import Link from 'next/link';
+import { Button } from 'antd';
+import { LuSettings } from 'react-icons/lu';
+import { ListItemText } from '@mui/material';
 // import Link from 'next/link';
 // import { Button } from 'antd';
 
@@ -10,42 +15,52 @@ export default function AppDrawer() {
         <Menu />
       </div>
       <div
-        className={'flex flex-row items-center justify-between gap-3 px-3 py-2'}
+        className={
+          'mb-2 flex flex-row items-center justify-between gap-3 px-3 py-2'
+        }
       >
-        <p>Work In-progress</p>
-        {/* <Image
-              variant="rounded"
-              src={display_picture}
-              {...stringAvatar(`${first_name} ${last_name}`)}
-              className="w-[56px] h-[56px] rounded-xl"
-            />
-            <ListItemText
-              primary={`${first_name} ${last_name}`}
-              secondary={
-                role ? (
-                  <>
-                    {role?.name} <br />
-                    {role?.id}
-                    {role?.name?.[0]}-{id}
-                  </>
-                ) : (
-                  "No Role Assigned"
-                )
-              }
-              primaryTypographyProps={{
-                noWrap: true,
-                className: "font-bold text-sm",
-              }}
-              secondaryTypographyProps={{
-                noWrap: true,
-                className: "text-xs leading-tight font-semibold",
-              }}
-            />
-            <Link to={"/app/settings"}>
-              <Button color="secondary">
-                <Icon icon={"solar:settings-bold-duotone"} />
-              </Button>
-            </Link> */}
+        {/* Dummy Profile UI.*/}
+        <div className='flex flex-row items-center gap-4'>
+          <Image
+            src='/dummyprofile.jpg'
+            alt='user profile picture'
+            width={56}
+            height={56}
+            className='rounded-lg'
+          />
+          <div>
+            <h1 className='text-base font-bold'>Abdus Satter</h1>
+            <p className='text-sm font-medium leading-tight'>Frontend Dev</p>
+          </div>
+        </div>
+
+        {/* <ListItemText
+          primary={`${'first_name'} ${'last_name'}`}
+          secondary={
+            role ?
+           ( <>
+              {role?.name} <br />
+                {role?.id}
+                {role?.name?.[0]}-{id}
+            </>)
+            : (
+              'No Role Assigned'
+            )
+          }
+          primaryTypographyProps={{
+            noWrap: true,
+            className: 'font-bold text-sm',
+          }}
+          secondaryTypographyProps={{
+            noWrap: true,
+            className: 'text-xs leading-tight font-semibold',
+          }}
+        /> */}
+        <Link href={'/settings'}>
+          <Button>
+            <LuSettings />
+          </Button>
+        </Link>
       </div>
     </>
   );
